8c151b8178d693824599d1677f9a7904eceaddd9
samuel rodrigues <samuel.rodrigues@acct.global>
2021-04-04 11:28:18 -0300

    feat: stateToProps to component of the sum

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,23 +1,22 @@
-import './Card.css'
-import React from 'react' 
+import "./Card.css";
+import React from "react";
 
 const getColor = (props) => {
-    if(props.Red) return "Red" 
-    if(props.Green) return "Green" 
-    if(props.Blue) return "Blue" 
-    if(props.Purple) return "Purple" 
-    return ""
-}
+  if (props.Red) return "Red";
+  if (props.Green) return "Green";
+  if (props.Blue) return "Blue";
+  if (props.Purple) return "Purple";
+  return "";
+};
 
-export default props => {
-    return (
-        <div className={`Card ${getColor(props)}`}>
-            <div className="Header">
-                    <span className="title">{ props.Title }</span>
-            </div>
-            <div className="Content">
-                    { props.children }
-            </div>
-        </div>
-    )
-}
+// eslint-disable-next-line import/no-anonymous-default-export
+export default (props) => {
+  return (
+    <div className={`Card ${getColor(props)}`}>
+      <div className="Header">
+        <span className="title">{props.Title}</span>
+      </div>
+      <div className="Content">{props.children}</div>
+    </div>
+  );
+};
--- src/components/Soma.jsx
+++ src/components/Soma.jsx
@@ -1,17 +1,28 @@
-import './Intervalo.css'
-import React from 'react' 
-import Card from './Card'
+/* eslint-disable import/no-anonymous-default-export */
+import "./Intervalo.css";
+import React from "react";
+import Card from "./Card";
+import { connect } from "react-redux"; //ao state da aplicação
 
-export default props => {
-    return (
-        <Card Title="Soma dos Números" Blue>
-            <div>
-                <span>
-                    <span> Resultado </span>
-                    <strong> 10 </strong>
-                </span>
-            </div>
-        </Card>
-    )
-}
+const Soma = (props) => {
+  const { max, min } = props.numerosMaxMin;
 
+  return (
+    <Card Title="Soma dos Números" Blue>
+      <div>
+        <span>
+          <span> Resultado </span>
+          <strong> {max + min} </strong>
+        </span>
+      </div>
+    </Card>
+  );
+};
+
+const mapStateToProps = (state) => {
+  return {
+    numerosMaxMin: state.numeros,
+  };
+};
+
+export default connect(mapStateToProps)(Soma);
